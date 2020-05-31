c4ccb095ea18cf8ba670730e08dba7f008328e45
Mohammad Ahsan <ahsan1fida@gmail.com>
2020-06-01 01:32:45 +0500

    staged necessary files to commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+//import 'package:flutter/flutter_chat_ui/screens/home_screen.dart';
+import './screens/home_screen.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Color(0xFFFEF9EB),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
